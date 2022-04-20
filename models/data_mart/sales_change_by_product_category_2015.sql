4f509100274db7acbd0da089dbd79733d896227c
Dmitriy Stepanenko <d.step.co@gmail.com>
2022-04-20 14:27:41 +0000

    small fixes

--- models/data_mart/sales_change_by_product_category_2015.sql
+++ models/data_mart/sales_change_by_product_category_2015.sql
@@ -10,43 +10,43 @@ SELECT
   , ROUND(SUM(sales_usd_2015) - SUM(sales_usd_2014), 2) as year_change_sales_usd
 FROM
     (
-SELECT
-    p.product_category_name             as category
-  , p.product_sub_category_name         as sub_category
-  , SUM(o.order_quantity)               as quantity_2014
-  , ROUND(SUM(o.order_sales_usd), 2)    as sales_usd_2014
-  , null                                as quantity_2015
-  , null                                as sales_usd_2015
-FROM
-    {{ source('data_storage', 'order') }} o
-LEFT JOIN
-    {{ source('data_storage', 'product') }} p
-    ON o.order_product_key = p.product_key
-WHERE
-    EXTRACT(YEAR FROM o.order_date) =2014
-GROUP BY
-    p.product_category_name
-  , p.product_sub_category_name
+    SELECT
+        p.product_category_name             as category
+      , p.product_sub_category_name         as sub_category
+      , SUM(o.order_quantity)               as quantity_2014
+      , ROUND(SUM(o.order_sales_usd), 2)    as sales_usd_2014
+      , null                                as quantity_2015
+      , null                                as sales_usd_2015
+    FROM
+        {{ source('data_storage', 'order') }} o
+    LEFT JOIN
+        {{ source('data_storage', 'product') }} p
+        ON o.order_product_key = p.product_key
+    WHERE
+        EXTRACT(YEAR FROM o.order_date) =2014
+    GROUP BY
+        p.product_category_name
+      , p.product_sub_category_name
 
-UNION ALL
+    UNION ALL
 
-SELECT
-    p.product_category_name             as category
-  , p.product_sub_category_name         as sub_category
-  , null                                as quantity_2014
-  , null                                as sales_usd_2014
-  , SUM(o.order_quantity)               as quantity_2015
-  , ROUND(SUM(o.order_sales_usd), 2)    as sales_usd_2015
-FROM
-    {{ source('data_storage', 'order') }} o
-LEFT JOIN
-    {{ source('data_storage', 'product') }} p
-    ON o.order_product_key = p.product_key
-WHERE
-    EXTRACT(YEAR FROM o.order_date) = 2015
-GROUP BY
-    p.product_category_name
-  , p.product_sub_category_name
+    SELECT
+        p.product_category_name             as category
+      , p.product_sub_category_name         as sub_category
+      , null                                as quantity_2014
+      , null                                as sales_usd_2014
+      , SUM(o.order_quantity)               as quantity_2015
+      , ROUND(SUM(o.order_sales_usd), 2)    as sales_usd_2015
+    FROM
+        {{ source('data_storage', 'order') }} o
+    LEFT JOIN
+        {{ source('data_storage', 'product') }} p
+        ON o.order_product_key = p.product_key
+    WHERE
+        EXTRACT(YEAR FROM o.order_date) = 2015
+    GROUP BY
+        p.product_category_name
+      , p.product_sub_category_name
     )
 GROUP BY
     category
