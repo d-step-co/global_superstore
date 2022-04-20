ea6360fa66a60afaa04d8008d817bcc176feb2c2
Dmitriy Stepanenko <d.step.co@gmail.com>
2022-04-20 07:15:34 +0000

    optimize merge conditions

--- models/etl_dimension/market.sql
+++ models/etl_dimension/market.sql
@@ -4,7 +4,12 @@
 ) }}
 
 SELECT DISTINCT
-    CONCAT(UPPER(LEFT(market, 2)),'-', LEFT(REGEXP_REPLACE(TO_HEX(MD5(market)), r"[^0-9]+", ""), 4)) as market_key    
-  , market as market_name
+    CONCAT(UPPER(LEFT(o.market, 2)),'-', LEFT(REGEXP_REPLACE(TO_HEX(MD5(o.market)), r"[^0-9]+", ""), 4)) as market_key    
+  , o.market as market_name
 FROM
-    {{ source('data_source', 'tmp_orders') }}
+    {{ source('data_source', 'tmp_orders') }} o
+WHERE
+    NOT EXISTS (SELECT m.market_key 
+                FROM {{ source('data_storage', 'market') }} m
+                WHERE m.market_key = CONCAT(UPPER(LEFT(o.market, 2)),'-', LEFT(REGEXP_REPLACE(TO_HEX(MD5(o.market)), r"[^0-9]+", ""), 4))
+                )
--- models/etl_dimension/product.sql
+++ models/etl_dimension/product.sql
@@ -4,9 +4,14 @@
 ) }}
 
 SELECT DISTINCT
-    product_id      as product_key
-  , product_name
-  , category        as product_category_name
-  , sub_category    as product_sub_category_name
+    o.product_id      as product_key
+  , o.product_name
+  , o.category        as product_category_name
+  , o.sub_category    as product_sub_category_name
 FROM
-    {{ source('data_source', 'tmp_orders') }}
+    {{ source('data_source', 'tmp_orders') }} o
+WHERE
+    NOT EXISTS (SELECT p.product_key 
+                FROM {{ source('data_storage', 'product') }} p
+                WHERE p.product_key = o.product_id
+                )
